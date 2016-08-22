aafc55ac0b4aeb8fbda1fc246b4e68dbc7d74d31
Randy Grewal <rgrewalucsb@gmail.com>
2016-08-22 09:24:22 -0700

    No errors when trying to seed, issue fixed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160819211506) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "summaries", force: true do |t|
     t.string  "location"
     t.integer "number_of_job_orders"
